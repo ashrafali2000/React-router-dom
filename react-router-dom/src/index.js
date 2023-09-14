5925cedaff386bffdae02dbbbe22bad1e466e227
Ashraf-ali-2000 <ali4282271@gmail.com>
2023-09-14 13:46:14 +0500

    indexjs page

--- react-router-dom/src/index.js
+++ react-router-dom/src/index.js
@@ -4,16 +4,13 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
-import Layout from './Layout';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   // <React.StrictMode>
   <BrowserRouter>
 
-  <Layout />
   <App />
-  
   </BrowserRouter>
   //  </React.StrictMode> 
 
