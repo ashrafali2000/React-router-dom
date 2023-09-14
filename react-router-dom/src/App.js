7df9b9ae5c92b01f3ec368900906a9ffc34b3e1e
Ashraf-ali-2000 <ali4282271@gmail.com>
2023-09-14 13:44:26 +0500

    app.js page

--- react-router-dom/src/App.js
+++ react-router-dom/src/App.js
@@ -1,20 +1,40 @@
-// import './App.css';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import {  Route, Routes} from "react-router-dom"
 import Home from "./Home";
-
+import Address from "./about/address";
 import Dashboard from "./Dashboard";
 import Layout from "./Layout";
 import About from "./about";
+import Contact from "./about/contact";
+import Information from "./about/address/information";
+import Street from "./about/address/streetNo";
 function App() {
   return (
     <div>
-     <div> My react roueter App
-      </div>
    <Routes>
-   <Route path="/" element={<Layout></Layout>} />
+   <Route path="/" element={<Layout></Layout>} >
    <Route index element={<Home></Home>} />
-   <Route path="about" element={<About></About>} />
+
+{/* nested routes */}
+   <Route path="about/" element={""}>
+   <Route index element={<About></About>} />
+   <Route path="contact" element={<Contact></Contact>}/>
+
+   {/* dobule nestd routes */}
+  <Route path="address/" element={""}>
+ <Route index element={<Address></Address>} />
+ <Route path="information" element={<Information></Information>} />
+ <Route path="street" element={<Street></Street>}/>
+  </Route>
+
+   </Route>
+
+
+
    <Route path="Dashboard" element={<Dashboard></Dashboard>} />
+
+   </Route>
    </Routes>
   
     </div>
