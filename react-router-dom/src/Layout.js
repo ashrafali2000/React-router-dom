b143b57118a6df6d91ae5d16f2551de248990fd3
Ashraf-ali-2000 <ali4282271@gmail.com>
2023-09-14 13:45:53 +0500

    navbar page

--- react-router-dom/src/Layout.js
+++ react-router-dom/src/Layout.js
@@ -1,12 +1,81 @@
-import { Link } from "react-router-dom";
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import Offcanvas from 'react-bootstrap/Offcanvas';
+import { Outlet } from 'react-router-dom';
 
-export default  function Layout(){
-    return(<div>
-        <h1>Logo</h1><h1>NavBar</h1>
-<div>
-    <Link to={"/"}>Home</Link>
-    <Link to= {"/about"}>About</Link>
-    <Link to= {"/Dashboard"}>Dashboard</Link>
-</div>
-    </div>)
+function Layout() {
+  return (
+    <>
+      {['sm'].map((expand) => (
+        <Navbar key={expand} expand={expand} className=" mb-3">
+          <Container fluid >
+            <Navbar.Brand href="#">React-router-dom</Navbar.Brand>
+            <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${expand}`} />
+            <Navbar.Offcanvas
+              id={`offcanvasNavbar-expand-${expand}`}
+              aria-labelledby={`offcanvasNavbarLabel-expand-${expand}`}
+              placement="end"
+            >
+              <Offcanvas.Header closeButton>
+                <Offcanvas.Title id={`offcanvasNavbarLabel-expand-${expand}`}>
+                  Offcanvas
+                </Offcanvas.Title>
+              </Offcanvas.Header>
+              <Offcanvas.Body>
+                <Nav className="justify-content-end flex-grow-1 pe-3">
+                  <Nav.Link href="/">Home</Nav.Link>
+                  <Nav.Link href="dashboard">Dashboard</Nav.Link>
+                  <NavDropdown
+                    title="About"
+                    id={`offcanvasNavbarDropdown-expand-${expand}`}
+                  >
+                    <NavDropdown.Item href="about">About</NavDropdown.Item>
+                    <NavDropdown.Item href="about/contact">
+                      Contact
+                    </NavDropdown.Item>
+                    <NavDropdown.Divider />
+                    <NavDropdown.Item href="#action5">
+                      Something else here
+                    </NavDropdown.Item>
+                  </NavDropdown>
+                  <NavDropdown
+                    title="Address"
+                    id={`offcanvasNavbarDropdown-expand-${expand}`}
+                  >
+                    <NavDropdown.Item href="about/address">Address</NavDropdown.Item>
+                    <NavDropdown.Item href="about/address/information">
+                      Information
+                    </NavDropdown.Item>
+                    <NavDropdown.Item href="about/address/street">
+                      Street
+                    </NavDropdown.Item>
+                    <NavDropdown.Divider />
+                    <NavDropdown.Item href="#action5">
+                      Something else here
+                    </NavDropdown.Item>
+                  </NavDropdown>
+                </Nav>
+                <Form className="d-flex">
+                  <Form.Control
+                    type="search"
+                    placeholder="Search"
+                    className="me-2"
+                    aria-label="Search"
+                  />
+                  <Button variant="outline-success">Search</Button>
+                </Form>
+              </Offcanvas.Body>
+            </Navbar.Offcanvas>
+          </Container>
+        </Navbar>
+      ))}
+      <Outlet></Outlet>
+    </>
+  );
 }
+
+export default Layout;
